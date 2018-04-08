713c4e2802f40fa2b092dcddd81230b9a7b5712d
Nick Wilson <thenickwilson@gmail.com>
2018-04-08 16:02:46 +0100

    User story 4 - final step
    Use the guesser when we get an invalid or erroneous account number,
    and amend the report accordingly.

--- src/main/scala/Reporter.scala
+++ src/main/scala/Reporter.scala
@@ -1,15 +1,29 @@
-class Reporter(parser: Parser, validator: Validator) {
+class Reporter(parser: Parser, validator: Validator, guesser: Guesser) {
 
   def prepareEntry(entry: Entry): String =
     parser.readEntry(entry) match {
 
-      case illegible  if illegible.contains('?') =>
-        illegible + " ILL"
-
       case validEntry if validator.checksum(validEntry) =>
         validEntry
 
-      case badChecksum =>
-        badChecksum + " ERR"
+      case invalid =>
+        val guesses =
+          guesser
+            .guesses(entry)
+            .map(parser.readEntry)
+
+        val feasibleGuesses =
+          guesses
+            .filterNot(_.contains('?'))
+            .filter(validator.checksum)
+
+        feasibleGuesses match {
+          case List(singlePossibility) => singlePossibility
+          case List()                  => invalid + " ILL"
+          case _                       => invalid + " AMB [" + prepareAmbiguousList(feasibleGuesses) + "]"
+        }
     }
+
+  private def prepareAmbiguousList(guesses: List[String]): String =
+    guesses.map(guess => s"'$guess'").mkString(", ")
 }
--- src/test/scala/ReporterSpec.scala
+++ src/test/scala/ReporterSpec.scala
@@ -2,7 +2,7 @@ import org.scalatest.{MustMatchers, WordSpec}
 
 class ReporterSpec extends WordSpec with MustMatchers {
 
-  val reporter = new Reporter(new Parser, new Validator)
+  val reporter = new Reporter(new Parser, new Validator, new Guesser)
 
   ".prepareEntry" must {
 
@@ -26,14 +26,74 @@ class ReporterSpec extends WordSpec with MustMatchers {
       reporter.prepareEntry(entry) mustEqual "?57?08000 ILL"
     }
 
-    "return the account number and ERR for a legible account that fails the checksum" in {
-      val entry = Entry(
-        " _  _     _  _        _  _ ",
-        "|_ |_ |_| _|  |  ||_||_||_ ",
-        "|_||_|  | _|  |  |  | _| _|"
+    "return the corrected account number for an illegible account number with one valid guess" in {
+      val entry = new Entry(
+        "    _  _  _  _  _  _  _  _ ",
+        " _||_   ||_ | ||_|| || || |",
+        "  | _|  | _||_||_||_||_||_|"
       )
 
-      reporter.prepareEntry(entry) mustEqual "664371495 ERR"
+      reporter.prepareEntry(entry) mustEqual "457508000"
+    }
+
+    "return 711111111 when given 111111111" in {
+      val entry = new Entry(
+        "                           ",
+        "  |  |  |  |  |  |  |  |  |",
+        "  |  |  |  |  |  |  |  |  |"
+      )
+
+      reporter.prepareEntry(entry) mustEqual "711111111"
+    }
+
+    "return 777777177 when given 777777777" in {
+      val entry = new Entry(
+        " _  _  _  _  _  _  _  _  _ ",
+        "  |  |  |  |  |  |  |  |  |",
+        "  |  |  |  |  |  |  |  |  |"
+      )
+
+      reporter.prepareEntry(entry) mustEqual "777777177"
+    }
+
+    "return 200800000 when given 2000000000" in {
+      val entry = new Entry(
+        " _  _  _  _  _  _  _  _  _ ",
+        " _|| || || || || || || || |",
+        "|_ |_||_||_||_||_||_||_||_|"
+      )
+
+      reporter.prepareEntry(entry) mustEqual "200800000"
+    }
+
+    "return 888888888 AMB ['888886888', '888888880', '888888988'] when given 888888888" in {
+      val entry = new Entry(
+        " _  _  _  _  _  _  _  _  _ ",
+        "|_||_||_||_||_||_||_||_||_|",
+        "|_||_||_||_||_||_||_||_||_|"
+      )
+
+      reporter.prepareEntry(entry) mustEqual "888888888 AMB ['888886888', '888888988', '888888880']"
+    }
+
+    "return 555555555 AMB ['559555555', '555655555'] when given 555555555" in {
+      val entry = new Entry(
+        " _  _  _  _  _  _  _  _  _ ",
+        "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
+        " _| _| _| _| _| _| _| _| _|"
+      )
+
+      reporter.prepareEntry(entry) mustEqual "555555555 AMB ['559555555', '555655555']"
+    }
+
+    "return 490867715 when given 49086771?" in {
+      val entry = new Entry(
+        line1 = "    _  _  _  _  _  _     _ ",
+        line2 = "|_||_|| ||_||_   |  |  | _ ",
+        line3 = "  | _||_||_||_|  |  |  | _|"
+      )
+
+      reporter.prepareEntry(entry) mustEqual "490867715"
     }
   }
 }
